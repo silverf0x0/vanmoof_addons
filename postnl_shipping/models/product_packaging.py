541d29b830b3a1dda6e189597c6a68553485e7e4
raffo <test@test.it>
2022-03-03 11:05:36 +0100

    [RM] Removed unused product_packaging model

--- postnl_shipping/models/product_packaging.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ProductPackaging(models.Model):
-    _inherit = 'product.packaging'
-    
-    package_carrier_type = fields.Selection(selection_add=[('delivery_postnl', 'PostNL')])
-    
